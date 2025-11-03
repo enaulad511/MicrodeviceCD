1d407c80d88fa0bf48f743739551c79881fb15f4
L03508427 <edisson.naulad@tec.mx>
2025-11-03 16:32:07 -0600

    frame creation ui square wave voltammetry

--- test.py
+++ test.py
@@ -50,19 +50,19 @@ if __name__ == "__main__":
     motor = MotorBTS7960(en=23)  # Descomenta si usas el motor
     hilo_uart = threading.Thread(target=leer_uart, daemon=True)
     try:
-        motor.avanzar(50)
+        # motor.avanzar(50)
         # Inicia hilo de lectura UART
         # hilo_uart = threading.Thread(target=leer_uart, daemon=True)
-        # hilo_uart.start()
-        # time.sleep(1)
+        hilo_uart.start()
+        time.sleep(1)
 
         # Inicia entrada de comandos por consola
         # enviar_comandos()
         while True:
             # Ejemplo de uso del motor
-            motor.avanzar(50)
+            motor.avanzar(15)
             time.sleep(1)
-            motor.retroceder(75)
+            motor.retroceder(25)
             time.sleep(1)
             motor.detener()
             time.sleep(1)
