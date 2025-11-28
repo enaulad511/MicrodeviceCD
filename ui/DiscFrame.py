0048accf126dd66fe79348fd47cc5412ada8090d
L03508427 <edisson.naulad@tec.mx>
2025-11-28 15:28:37 -0600

    add max and min values in pid settings

--- ui/DiscFrame.py
+++ ui/DiscFrame.py
@@ -149,7 +149,7 @@ def spinMotorRPM(direction, rpm, ts):
         ki=pid_cfg["ki"],
         kd=pid_cfg["kd"],
         setpoint=rpm,
-        output_limits=(10, 50),
+        output_limits=(pid_cfg.get(min, 10), pid_cfg.get("max", 50)),
         ts=ts,
     )
     current_time = time.perf_counter()
