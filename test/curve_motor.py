866762c65ac7324d0fc99d8cb61e26fa255da513
L03508427 <edisson.naulad@tec.mx>
2025-11-28 17:03:50 -0600

    fix line busy

--- test/curve_motor.py
+++ test/curve_motor.py
@@ -3,11 +3,22 @@ import time
 from templates.constants import serial_port_encoder
 from Drivers.EncoderData import EncoderData
 from Drivers.DriverMotorDC import MotorBTS7960
+import signal
+import sys
 
 __author__ = "Edisson Naula"
 __date__ = "$ 28/11/2025 at 15:59 $"
 
+
 motor = MotorBTS7960(en=23)
+
+def handler(sig, frame):
+    motor.limpiar()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
+signal.signal(signal.SIGTERM, handler)
+
 
 if __name__ == "__main__":
     t_test = 10.0  # seconds
