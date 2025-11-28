38102ebefbc5464e29dacdaf969c785c13c6175d
L03508427 <edisson.naulad@tec.mx>
2025-11-28 16:42:42 -0600

    fix passedtime curve mtoro

--- test/curve_motor.py
+++ test/curve_motor.py
@@ -10,12 +10,13 @@ __date__ = "$ 28/11/2025 at 15:59 $"
 motor = MotorBTS7960(en=23)
 
 if __name__ == "__main__":
-    t_test = 10  # seconds
+    t_test = 10.0  # seconds
     t_s = 0.01  # seconds
     print ("Test started")
     current_time = time.perf_counter()
     data_encoder = EncoderData(serial_port_encoder, 115200)
-    while current_time < t_test:
+    passed_time = 0.0
+    while passed_time < t_test:
         raw_data = data_encoder.leer_uart()
         data_encoder.parse_line(raw_data)
         rpm_actual = data_encoder.get_rpm()
@@ -23,4 +24,5 @@ if __name__ == "__main__":
         print(f"RPM actual: {rpm_actual}")
         while time.perf_counter() - current_time < t_s:
             pass
+        passed_time += time.perf_counter() - current_time
     print("Test finished")
