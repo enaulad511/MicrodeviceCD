f7683807a55060cfc9b0829887095e12a4d9603c
Edisson Naula <136759460+ENtelintec@users.noreply.github.com>
2026-02-19 15:30:27 -0600

    add test file ads

---
+++ test/test_ads.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from Drivers.ReaderADS import Ads1115Reader
+from ui.PcrFrame import ads
+
+__author__ = "Edisson Naula"
+__date__ = "$ 19/02/2026 at 15:29 $"
+
+
+if __name__ == "__main__":
+    ads = Ads1115Reader(address=0x4A, fsr=4.096, sps=128, single_shot=True)
+    for i in range(10):
+        print(ads.read_voltage(0, averages=4))
