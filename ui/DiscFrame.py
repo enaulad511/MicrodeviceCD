1d119ef8ee0b8f45697143aaf2549eb9234cddbc
Edisson Naula <136759460+ENtelintec@users.noreply.github.com>
2026-03-03 15:57:11 -0600

    debug

--- ui/DiscFrame.py
+++ ui/DiscFrame.py
@@ -287,7 +287,7 @@ def spinMotorRPM_ramped(
     status = drv.get_status()  # pyrefly: ignore
     print("status ", status)
     
-    complete_rotation = int(360 - status.get('pos_deg'))
+    complete_rotation = int(360 - status.get('pos_deg') % 360)
     print(f"Rotación completa para volver a 0°: {complete_rotation}°")
     drv.move_degrees(complete_rotation, wait=True)  # pyrefly: ignore
     if drv is not None:
