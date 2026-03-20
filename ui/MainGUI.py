632e928179990150217ee6c2c763f46b990c0919
Edisson Naula <136759460+ENtelintec@users.noreply.github.com>
2026-03-20 11:44:29 -0600

    fix gain

--- ui/MainGUI.py
+++ ui/MainGUI.py
@@ -101,7 +101,7 @@ class MainGUI(ttk.Window):
         self.ads = None
         if secrets.get("environment", "") != "dev":
             from Drivers.ReaderADS import Ads1115Reader
-            self.ads = Ads1115Reader(address=0x48, fsr=1.024, sps=128, single_shot=True) 
+            self.ads = Ads1115Reader(address=0x48, fsr=6.144, sps=128, single_shot=True) 
         # --------------------Start Animation -------------------
         # self.show_gif_toplevel()
         self.after(0, self.maximize_window)
