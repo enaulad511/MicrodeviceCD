7a92930740d91cda23cd369e80fb836f104f9405
Edisson Naula <136759460+ENtelintec@users.noreply.github.com>
2026-04-29 11:00:25 -0600

    fgix y lim

--- ui/PcrFrame.py
+++ ui/PcrFrame.py
@@ -347,7 +347,7 @@ class PCRFrame(ttk.Frame):
         # self.ax.autoscale_view(scalex=False, scaley=True)
         
         # Eje Y fijo
-        self.ax.set_ylim(0, 100)
+        self.ax.set_ylim(-5, 105)
 
 
         self.canvas.draw_idle()
