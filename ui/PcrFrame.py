8ee8553a350fcdb3ab5390f112bbf598f2ece256
Edisson Naula <136759460+ENtelintec@users.noreply.github.com>
2026-05-07 15:32:27 -0600

    fix pcr frame psosition

--- ui/PcrFrame.py
+++ ui/PcrFrame.py
@@ -41,7 +41,7 @@ def create_widgets_pcr(parent):
 
     # Frame: Configuración PCR
     frame1 = ttk.LabelFrame(parent, text="PCR Configuration")
-    frame1.grid(row=0, column=0, padx=10, pady=10, sticky="nswe")
+    frame1.grid(row=0, column=0, padx=(5, 25), pady=(5, 5), sticky="nswe")
     # frame1.configure(style="Custom.TLabelframe")
 
     labels = [
